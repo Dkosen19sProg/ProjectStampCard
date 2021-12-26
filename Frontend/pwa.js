93fe82ea01b68d75f875359fce2c342fcb27e8b6
119311 <r119311@g.kobe-kosen.ac.jp>
2021-12-26 23:58:22 +0900

    test

--- Frontend/pwa.js
+++ Frontend/pwa.js
@@ -1,9 +1,12 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-        //登録完了
-        console.log('登録成功');
-    }).catch(function(err) {
-        //登録失敗
-        console.log('失敗');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then(function (registration) {
+      //登録完了
+      console.log("登録成功");
     })
+    .catch(function (err) {
+      //登録失敗
+      console.log("失敗");
+    });
 }
