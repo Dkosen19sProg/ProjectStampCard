16f99ab3dc91085d14c4a3f0937e72ed4ad72e5c
irimoti <52734487+r119306@users.noreply.github.com>
2021-12-29 00:39:36 +0900

    sample_sql

--- Frontend/databaseTest.php
+++ Frontend/databaseTest.php
@@ -1,3 +1,17 @@
 <?php
-echo 'hello'
+
+$pdo = new PDO('mysql:host=localhost;dbname=foo_db', 'user', 'pass');
+
+$pdo->query('CREATE TABLE foo_table (id INT, name VARCHAR(20))');
+
+$pdo->query('INSERT INTO foo_table (id, name) VALUES (1,"yamada")');
+
+$result_rows = $pdo->query('SELECT * FROM foo_table');
+
+foreach ( $result_rows as $row ) {
+    // テーブルの1行ごとに1つの配列として格納されている（$row）
+    // その1行ごとの配列内で、列名をキーにした連想配列でデータが格納されている。
+    echo "id: {$row['id']}"; // id列
+    echo "name: {$row['name']}"; // name列
+}
 ?>
