05b46730f3bb9632f6ca840de6cc784289c5382e
irimoti <52734487+r119306@users.noreply.github.com>
2021-12-29 15:03:11 +0900

    SQL接続記述_localhost_成功

--- Frontend/databaseTest.php
+++ Frontend/databaseTest.php
@@ -1,17 +1,27 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=foo_db', 'user', 'pass');
+// $pdo = new PDO('mysql:host=localhost;dbname=foo_db', 'user', 'pass');
 
-$pdo->query('CREATE TABLE foo_table (id INT, name VARCHAR(20))');
+// $pdo->query('CREATE TABLE foo_table (id INT, name VARCHAR(20))');
 
-$pdo->query('INSERT INTO foo_table (id, name) VALUES (1,"yamada")');
+// $pdo->query('INSERT INTO foo_table (id, name) VALUES (1,"yamada")');
 
-$result_rows = $pdo->query('SELECT * FROM foo_table');
+// $result_rows = $pdo->query('SELECT * FROM foo_table');
 
-foreach ( $result_rows as $row ) {
-    // テーブルの1行ごとに1つの配列として格納されている（$row）
-    // その1行ごとの配列内で、列名をキーにした連想配列でデータが格納されている。
-    echo "id: {$row['id']}"; // id列
-    echo "name: {$row['name']}"; // name列
+// foreach ( $result_rows as $row ) {
+//     // テーブルの1行ごとに1つの配列として格納されている（$row）
+//     // その1行ごとの配列内で、列名をキーにした連想配列でデータが格納されている。
+//     echo "id: {$row['id']}"; // id列
+//     echo "name: {$row['name']}"; // name列
+// }
+
+
+$link = mysqli_connect('localhost', 'root', 'd3pro2group6', 'shop');
+
+// 接続状況をチェックします
+if (mysqli_connect_errno()) {
+    die("データベースに接続できません:" . mysqli_connect_error() . "\n");
+} else {
+    echo "データベースの接続に成功しました。\n";
 }
 ?>
