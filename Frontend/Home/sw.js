d19eb1ff8ea482137c57646cd2031f4b91230749
irimoti <52734487+r119306@users.noreply.github.com>
2022-02-03 11:19:00 +0900

    最新のmainを持ってきた

---
+++ Frontend/Home/sw.js
@@ -0,0 +1,23 @@
+// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+
+// キャッシュ名とキャッシュファイルの指定
+const CACHE_NAME = "pwa-sample-caches";
+var urlsToCache = ["/index.html", "/js/camera.js"];
+
+// インストール処理
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response ? response : fetch(event.request);
+    })
+  );
+});
---
+++ Frontend/js/camera.js
@@ -0,0 +1,24 @@
+function openCamera() {
+    var video = document.querySelector('video');
+    navigator.mediaDevices = navigator.mediaDevices
+    || ((navigator.mozGetUserMedia 
+    || navigator.webkitGetUserMedia) ? {
+        getUserMedia: function(c) {
+            return new Promise(function(y, n) {
+                (navigator.mozGetUserMedia ||
+                navigator.webkitGetUserMedia).call(navigator, c, y, n);
+            });
+        }
+    } : null);
+    var constraints = { video: { facingMode: 'environment', width: 1280, height: 720 } };
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) {
+            video.srcObject = stream;
+            video.onloadedmetadata = function(e) {
+                video.play();
+            };
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
