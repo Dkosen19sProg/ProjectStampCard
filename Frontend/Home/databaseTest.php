3ef6f1718d893e3767acd2b2131c1e0d89d5c8c8
119311 <r119311@g.kobe-kosen.ac.jp>
2022-02-03 12:03:35 +0900

    test

--- Frontend/Home/databaseTest.php
+++ Frontend/Home/databaseTest.php
@@ -7,30 +7,32 @@ function console_log($data)
     echo '</script>';
 }
 
-$link = mysqli_connect('localhost', 'group6', 'd3pro2group6', 'stamp');
-
+$db_link = mysqli_connect('localhost', 'webstampcard', '8Ud8', 'database_name');
 // 接続状況をチェックします
-if (mysqli_connect_errno()) {
+if (mysqli_connect_errno($db_link)) {
     die("データベースに接続できません:" . mysqli_connect_error() . "\n");
 } else {
     console_log("データベースの接続に成功しました。\n");
 }
 
+// // 接続解除
+// mysqli_close($db_link);
+
 // $query = "UPDATE user SET id = 2, name = 'yamato' WHERE id = 1;";
 
-// if (mysqli_query($link, $query)) {
+// if (mysqli_query($db_link, $query)) {
 //     console_log("UPDATE に成功しました \n");
 // }
 
 // $query = "INSERT INTO user (name) VALUES ('manekineko');";
 
-// if (mysqli_query($link, $query)) {
+// if (mysqli_query($db_link, $query)) {
 //     console_log("INSERT に成功しました。\n");
 // }
 
 // $query = "DELETE FROM user WHERE id IS NULL;";
 
-// if (mysqli_query($link, $query)) {
+// if (mysqli_query($db_link, $query)) {
 //     console_log("DELETE に成功しました。\n");
 // }else {
 
@@ -38,7 +40,7 @@ if (mysqli_connect_errno()) {
 console_log("店舗出力");
 $query = "SELECT id, shopName FROM RegisteredShops;";
 
-if ($result = mysqli_query($link, $query)) {
+if ($result = mysqli_query($db_link, $query)) {
     console_log("SELECT に成功しました。\n");
     foreach ($result as $row) {
         console_log($row);
@@ -47,7 +49,7 @@ if ($result = mysqli_query($link, $query)) {
 console_log("ユーザー出力");
 $query = "SELECT id, name, pass, createdDate FROM RegisteredCustomers;";
 
-if ($result = mysqli_query($link, $query)) {
+if ($result = mysqli_query($db_link, $query)) {
     console_log("SELECT に成功しました。\n");
     foreach ($result as $row) {
         console_log($row);
@@ -56,7 +58,7 @@ if ($result = mysqli_query($link, $query)) {
 console_log("カード出力");
 $query = "SELECT id, cardKey, stampCounts FROM StampCards;";
 
-if ($result = mysqli_query($link, $query)) {
+if ($result = mysqli_query($db_link, $query)) {
     console_log("SELECT に成功しました。\n");
     foreach ($result as $row) {
         console_log($row);
@@ -65,7 +67,7 @@ if ($result = mysqli_query($link, $query)) {
 console_log("inner join 出力");
 $query = "select stampCards.id,shopName,name,stampCounts,stampGoal from StampCards inner join RegisteredShops on StampCards.cardStoreId = RegisteredShops.id inner join RegisteredCustomers on StampCards.cardOwnerId = RegisteredCustomers.id;";
 
-if ($result = mysqli_query($link, $query)) {
+if ($result = mysqli_query($db_link, $query)) {
     console_log("SELECT に成功しました。\n");
     foreach ($result as $row) {
         console_log($row);
@@ -74,4 +76,4 @@ if ($result = mysqli_query($link, $query)) {
 
 
 // 接続を閉じます
-mysqli_close($link);
+mysqli_close($db_link);
